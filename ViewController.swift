dd8e70bb1a767229e6e367b9e77f673105cb77dc
luckysiqin <a20131105804@Mac28.local>
2016-03-28 21:33:38 +0800

    jisuanqi
    xiugai

--- ViewController.swift
+++ ViewController.swift
@@ -7,36 +7,61 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
- 
-  
+    
+    
     @IBOutlet weak var x: UITextField!
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
-    @IBAction func add(sender: UIButton) {
+    @IBAction func jisuan(sender: AnyObject) {
         var a:Double=0
         var b:Double=0
         var c:Double=0
         if(!x.text! .isEmpty){
-          a=(x.text! as NSString).doubleValue
-          }
-         if(!y.text!.isEmpty){
+            a=(x.text! as NSString).doubleValue
+        }
+        if(!y.text!.isEmpty){
             b=(y.text! as NSString).doubleValue
-           }
-          c=a+b
-          z.text="\(c)"
-         }
+        }
+        c=a+b
+        z.text="\(c)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func one(sender: AnyObject) {
+        x.text="1"
+    }
+    @IBAction func two(sender: AnyObject) {
+        x.text="2"
+    }
+    @IBAction func three(sender: AnyObject) {
+        x.text="3"
+    }
+    @IBAction func four(sender: AnyObject) {
+          x.text="4"
+    }
+    @IBAction func five(sender: AnyObject) {
+          x.text="5"
+    }
+    @IBAction func six(sender: AnyObject) {
+          x.text="6"
+    }
+    @IBAction func seven(sender: AnyObject) {
+          x.text="7"
+    }
+    @IBAction func eight(sender: AnyObject) {
+          x.text="8"
+    }
+    @IBAction func nine(sender: AnyObject) {
+          x.text="9"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
 }
-
-
+    
+}
