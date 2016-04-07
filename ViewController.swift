e11f5a7710bed569c66fd3e42a69925ca8ec6aa3
luckysiqin <a20131105804@Mac28.local>
2016-04-07 11:57:05 +0800

    计算器
    按钮设置

--- ViewController.swift
+++ ViewController.swift
@@ -8,59 +8,92 @@
 
 import UIKit
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var x: UITextField!
-    @IBOutlet weak var y: UITextField!
-    @IBAction func jisuan(sender: AnyObject) {
-        var a:Double=0
-        var b:Double=0
-        var c:Double=0
-        if(!x.text! .isEmpty){
-            a=(x.text! as NSString).doubleValue
+    var 计算器 = 计算()
+    var 显示缓存 =  " "
+    var 缓存 = 0.0
+    var 缓存2:Double?
+    @IBOutlet weak var 屏幕: UITextField!
+   
+    @IBAction func 数字9(sender: AnyObject) {
+        点按("9")
+    }
+    @IBAction func 数字8(sender: AnyObject) {
+         点按("8")
+    }
+    @IBAction func 数字7(sender: AnyObject) {
+         点按("7")
+    }
+    @IBAction func 数字6(sender: AnyObject) {
+         点按("6")
+    }
+    @IBAction func 数字5(sender: AnyObject) {
+         点按("5")
+    }
+    @IBAction func 数字4(sender: AnyObject) {
+         点按("4")
+    }
+    @IBAction func 数字3(sender: AnyObject) {
+         点按("3")
+    }
+    @IBAction func 数字2(sender: AnyObject) {
+         点按("2")
+    }
+    @IBAction func 数字1(sender: AnyObject) {
+         点按("1")
+    }
+    @IBAction func 数字0(sender: AnyObject) {
+         点按("0")
+    }
+    @IBAction func 小数点(sender: AnyObject) {
+         点按(".")
+    }
+    @IBAction func 算法加(sender: AnyObject) {
+        计算器.设置当前算法(.加法)
+        求值()
+    }
+    @IBAction func 算法减(sender: AnyObject) {
+        计算器.设置当前算法(.减法)
+        求值()
+    }
+    @IBAction func 算法乘(sender: AnyObject) {
+        计算器.设置当前算法(.乘法)
+        求值()
+    }
+    @IBAction func 算法除(sender: AnyObject) {
+        计算器.设置当前算法(.除法)
+        求值()
+    }
+    @IBAction func 重置当前(sender: AnyObject) {
+    }
+    @IBAction func 结果(sender: AnyObject) {
+          求值()
+    }
+    func 点按(数字:String) {
+        显示缓存 += 数字
+        屏幕.text = 显示缓存
+    }
+    func 求值() {
+        var 结果 = ""
+        
+        if !显示缓存.isEmpty {
+            let 临时 = 显示缓存 as NSString
+             缓存 =  临时.doubleValue
+            显示缓存 = ""
         }
-        if(!y.text!.isEmpty){
-            b=(y.text! as NSString).doubleValue
+        let 临时 = 缓存
+        
+        if let 前一个数字 = 缓存2{
+             结果 = 计算器.求结果(操作数: 前一个数字, 被操作数:临时)
+            屏幕.text =  结果
+            let 临 = 结果 as NSString
+            缓存2 = 临.doubleValue
+        }else{
+           缓存2 = 缓存
+            缓存 = 0.0
         }
-        c=a+b
-        y.text="\(c)"
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func one(sender: AnyObject) {
-        x.text="1"
-    }
-    @IBAction func two(sender: AnyObject) {
-        x.text="2"
-    }
-    @IBAction func three(sender: AnyObject) {
-        x.text="3"
-    }
-    @IBAction func four(sender: AnyObject) {
-          x.text="4"
-    }
-    @IBAction func five(sender: AnyObject) {
-          x.text="5"
-    }
-    @IBAction func six(sender: AnyObject) {
-          x.text="6"
-    }
-    @IBAction func seven(sender: AnyObject) {
-          x.text="7"
-    }
-    @IBAction func eight(sender: AnyObject) {
-          x.text="8"
-    }
-    @IBAction func nine(sender: AnyObject) {
-          x.text="9"
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
 }
+
     
-}
+
+    
